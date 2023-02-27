0bff0bc74961889cdb32ca174be363e49d8042ac
Dmitriy Kopaygora <dmitriy.kopaygora@ab-soft.net>
2023-02-27 16:59:33 +0100

    sync

--- lab1/init.w10.ps1
+++ lab1/init.w10.ps1
@@ -1,2 +1,15 @@
-choco install minikube virtualbox
+choco install minikube virtualbox eksctl
+#Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+#bcdedit /set hypervisorlaunchtype off
+#shutdown -r -t 1
+
 minikube start
+minikube status
+minikube ip
+
+kubectl get componentstatuses
+kubectl cluster-info
+kubectl get nodes
+#minikube stop
+#minikube delete
+minikube ssh
